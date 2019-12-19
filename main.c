7db0ccd5e0fce56abde62951a1a04ef8a2b132f6
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-12-19 18:29:37 +0300

    Improvements and bugfixes to the motor driver It works now for all 4 motors

--- main.c
+++ main.c
@@ -1,25 +1,15 @@
 #include "LPC407x_8x_177x_8x.h"
 #include "Library/GPIO.h"
-#include "Library/PWM.h"
 #include "Library/Joystick.h"
 #include "Library/LED.h"
-
-void EnableMotors() {
-	const uint32_t port1mask = (1<<20)|(1<<23)|(1<<24);
-	GPIO_DIR_Write(GPIO_PORT1,port1mask,OUTPUT);
-	GPIO_DIR_Write(GPIO_PORT0,1<<21,OUTPUT);
-	GPIO_PIN_Write(GPIO_PORT1,port1mask,HIGH);
-	GPIO_PIN_Write(GPIO_PORT0,1<<21,HIGH);
-}
+#include "Library/Motors.h"
 
 void init() {
 	//Joystick_Init();
-	EnableMotors();
-	PWM_Init();
+	Motors_Init();
 	LED_Init();
-	LED_OFF();
 	
-	PWM_Write(0, 0.75);
+	LED_OFF();
 	
 	//Initialize GPIO pins
 }
@@ -35,9 +25,18 @@ void update() {
 }
  
 int main() {
+		int i;
     init();
  
     while(1) {
-        update();
+				Motors_Set_Scaled_Speed(3, 1);
+				for (i=0; i<15;i++)
+					update();
+				Motors_Set_Scaled_Speed(3, -1);
+				for (i=0; i<15;i++)
+					update();
+				Motors_Set_Scaled_Speed(3, 0);
+				for (i=0; i<15;i++)
+					update();
     }
 }
