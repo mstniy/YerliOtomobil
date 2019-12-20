ee5d7e3113fe51ad69153f69d63770aa73a07d97
enes oncu <abdullahenesoncu@gmail.com>
2019-12-20 07:34:31 +0300

    led files are now like in master

--- Library/LED.h
+++ Library/LED.h
@@ -1,40 +1,33 @@
 #ifndef LED_H
 #define LED_H
 
+#include "LPC407x_8x_177x_8x.h"
 #include "GPIO.h"
 
-#define LEFT_LED_PORT                   GPIO_PORT2
-#define LEFT_LED_MASK                   ((uint32_t) 1 << 10)
-#define LEFT_LED_IOCON_ADDRESS          0x4002C128
-#define LEFT_LED_IOCON                  *((volatile uint32_t*)(LEFT_LED_IOCON_ADDRESS))
+#define LED1_PORT			GPIO_PORT1
+#define LED1_MASK			((uint32_t) 1 << 18)
 
-#define RIGHT_LED_PORT                  GPIO_PORT1
-#define RIGHT_LED_MASK                  ((uint32_t) 1 << 12)
-#define RIGHT_LED_IOCON_ADDRESS         0x4002C0B0
-#define RIGHT_LED_IOCON                 *((volatile uint32_t*)(RIGHT_LED_IOCON_ADDRESS))
+#define LED2_PORT			GPIO_PORT0
+#define LED2_MASK			((uint32_t) 1 << 13)
 
-#define FRONT_LED_PORT			        GPIO_PORT1
-#define FRONT_LED_MASK			        ((uint32_t) 1 << 11)
-#define FRONT_LED_IOCON_ADDRESS         0x4002C0AC
-#define FRONT_LED_IOCON                 *((volatile uint32_t*)(FRONT_LED_IOCON_ADDRESS))
+#define LED3_PORT			GPIO_PORT1
+#define LED3_MASK			((uint32_t) 1 << 13)
 
-#define BACK_LED_PORT                   GPIO_PORT1
-#define BACK_LED_MASK	                ((uint32_t) 1 << 7)
-#define BACK_LED_IOCON_ADDRESS          0x4002C09C
-#define BACK_LED_IOCON                  *((volatile uint32_t*)(BACK_LED_IOCON_ADDRESS))
+#define LED4_PORT			GPIO_PORT2
+#define LED4_MASK			((uint32_t) 1 << 19)
 
-void LEDs_Init(void);
+void LED_Init(void);
 
-void LEDs_ON(void);
-void LEFT_LED_On(void);
-void SAG_On(void);
-void FRONT_LED_On(void);
-void BACK_LED_On(void);
+void LED_ON(void);
+void LED1_On(void);
+void LED2_On(void);
+void LED3_On(void);
+void LED4_On(void);
 
-void LEDs_OFF(void);
-void LEFT_LED_Off(void);
-void RIGHT_LED_Off(void);
-void FRONT_LED_Off(void);
-void BACK_LED_Off(void);
+void LED_OFF(void);
+void LED1_Off(void);
+void LED2_Off(void);
+void LED3_Off(void);
+void LED4_Off(void);
 
 #endif
