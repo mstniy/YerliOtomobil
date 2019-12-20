ada97fd661ee37a043d5543f12f1c769e59ea017
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-12-20 23:57:30 +0300

    implement bluetooth

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "LPC407x_8x_177x_8x.h"
 #include "Library/GPIO.h"
 #include "Library/PWM.h"
+#include "Library/HM10.h"
 #include "Library/Joystick.h"
 #include "Library/Serial.h"
 
@@ -9,8 +10,32 @@ void init() {
 //	PWM_Init();
 	
 	Serial_Init();
+	HM10_Init();
 	
 	//Initialize GPIO pins
+}
+
+char buf[128];
+int i;
+volatile int x;
+
+void handle_command() {
+	buf[i++] = '\n';
+	buf[i++] = '\0';
+	HM10_Write(buf);
+	i = 0;
+
+	
+	for (x = 0; x < 1000000; x++);
+	
+	HM10Buffer[HM10CurrentBufferIndex] = 0;
+	strcpy(buf, HM10Buffer);
+	HM10_ClearBuffer();
+	serialTransmitData = buf;
+	Serial_WriteData(*serialTransmitData++);
+	while(!serialTransmitCompleted)
+		;
+	strcpy(buf, "");
 }
 
 void update() {
@@ -20,14 +45,20 @@ void update() {
 			;
 		
 		s[0] = serialReceivedCharacter;
-		if (s[0] == '\r') 
+		if (s[0] == '\r') { 
 			s[1] = '\n';
+		}
 		
-
+		buf[i++] = s[0];
+			
 		serialTransmitData = s;
 	  Serial_WriteData(*serialTransmitData++);
 		while(!serialTransmitCompleted)
 			;
+		
+		if (s[0] == '\r')
+			handle_command();
+
 		
 		serialNewDataAvailable = 0;
 }
