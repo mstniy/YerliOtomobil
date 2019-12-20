ed125cb7b1a7fc2e62fd8a665451e7e2070d3db4
Selim Karaduman <selim.karaduman07@gmail.com>
2019-12-20 14:25:39 +0300

    Update ADC.h

--- Library/ADC.h
+++ Library/ADC.h
@@ -25,16 +25,22 @@ typedef struct {
 #define	LDR_RIGHT_PIN_IOCON_ADDRESS	0x4002C068 //p0_26; ADC0[3]
 #define LDR_RIGHT_PIN_IOCON	*((volatile uint32_t*)(ANALOG_PIN_IOCON_ADDRESS))
 
+#define	POTENTIOMETER_PIN_IOCON_ADDRESS	0x4002C05C //IOCON_P0_23 P0[23] adc0[0]
+#define POTENTIOMETER_PIN_IOCON	*((volatile uint32_t*)(ANALOG_PIN_IOCON_ADDRESS))
+
 //Write the max value of ADC.
 #define ADC_MAX_VALUE 0x00000FFF
 
 //Define a ADC_CLKDIV variable for given ADC_CLOCK_FREQUENCY.
-//This ASSUMES 60 MHz PCLK frequency.
-#define ADC_CLKDIV 59
+//This ASSUMES 60 MHz PCLK frequency, with 2 ADC
+#define ADC_CLKDIV (PERIPHERAL_CLOCK_FREQUENCY/ADC_CLOCK_FREQUENCY)-1
 
 #define ADC_BASE	0x40034000
-#define ADC_LEFT	((ADC_TypeDef*) ADC_BASE)
-#define ADC_RIGHT	((ADC_TypeDef*) ADC_BASE)
+#define ADC	((ADC_TypeDef*) ADC_BASE)
+
+#define ADC_LEFT_CHANNEL 2
+#define ADC_RIGHT_CHANNEL 3
+#define ADC_POTENTIOMETER_CHANNEL 0
 
 extern uint32_t ADC_Last[];
 extern uint8_t ADC_New_Data_Available[];
