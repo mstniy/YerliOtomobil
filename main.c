9787366e088f6a2c61a0136fdff059100411e60d
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-12-20 14:34:08 +0300

    Ultrasonic driver fixes It now supports one ultrasonic sensor only It doesn't work

--- main.c
+++ main.c
@@ -3,18 +3,15 @@
 #include "Library/Joystick.h"
 #include "Library/LED.h"
 #include "Library/Motors.h"
+#include "Library/Ultrasonic.h"
 
 void init() {
-	//Joystick_Init();
 	Motors_Init();
+	Ultrasonic_Init();
 	LED_Init();
-	
-	LED_OFF();
-	
-	//Initialize GPIO pins
 }
 
-void update() {
+void led_blink() {
 	volatile int i;
 	LED1_On();
 	for (i=0; i<1000000; i++)
@@ -23,20 +20,62 @@ void update() {
 	for (i=0; i<1000000; i++)
 		;
 }
+
+void update_motor_test() {
+	int i;
+	Motors_Set_Scaled_Speed(3, 1);
+	for (i=0; i<15;i++)
+		led_blink();
+	Motors_Set_Scaled_Speed(3, -1);
+	for (i=0; i<15;i++)
+		led_blink();
+	Motors_Set_Scaled_Speed(3, 0);
+	for (i=0; i<15;i++)
+		led_blink();
+}
+
+void update_ultrasonic_test() {
+	double dist_cm, scaled_speed;
+	if (ultrasonicSensorNewDataAvailable == 0)
+		return ;
+	ultrasonicSensorNewDataAvailable = 0;
+	dist_cm = (ultrasonicSensorFallingCaptureTime-ultrasonicSensorRisingCaptureTime)/58.0;
+	if (dist_cm < 5) {
+		LED_ON();
+	}
+	else if (dist_cm < 10) {
+		LED1_On();
+		LED2_On();
+		LED3_On();
+		LED4_Off();
+	}
+	else if (dist_cm < 15) {
+		LED1_On();
+		LED2_On();
+		LED3_Off();
+		LED4_Off();
+	}
+	else if (dist_cm < 25) {
+		LED1_On();
+		LED2_Off();
+		LED3_Off();
+		LED4_Off();
+	}
+	else {
+		LED_OFF();
+	}
+	scaled_speed = dist_cm/50;
+	if (scaled_speed<0)
+		scaled_speed = 0;
+	if (scaled_speed > 1)
+		scaled_speed = 1;
+	Motors_Set_Scaled_Speed(3, scaled_speed);
+}
  
 int main() {
-		int i;
     init();
  
     while(1) {
-				Motors_Set_Scaled_Speed(3, 1);
-				for (i=0; i<15;i++)
-					update();
-				Motors_Set_Scaled_Speed(3, -1);
-				for (i=0; i<15;i++)
-					update();
-				Motors_Set_Scaled_Speed(3, 0);
-				for (i=0; i<15;i++)
-					update();
+				update_ultrasonic_test();
     }
 }
